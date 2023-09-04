a8deb42f0941f36dfc5584b0404f68740f22a23e
eduard <eduard42rus@mail.ru>
2023-09-04 22:08:15 +0700

    HW 4.3 final corrects have been done

--- src/main/java/ru/hogwarts/school/model/Avatar.java
+++ src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 @Entity
 public class Avatar {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String filePath;
     private long fileSize;
